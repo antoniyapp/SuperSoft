3c7644a7e5ec1fc323d0b710b69db3b60f2d1cf0
antoniyapp <antoniya.partacheva@abv.bg>
2017-03-20 23:07:27 +0200

    small fixes

--- transition-efect.js
+++ transition-efect.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $("div.story-part").click(function (event) {
         event.preventDefault();
-        linkLocation=this.getAttribute('to');
+        linkLocation=this.getAttribute('title');
         $(this).css('display','none');
         $(linkLocation).css('display','block');
-
+        
     })
 })
